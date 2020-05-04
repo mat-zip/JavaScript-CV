5349f16e2c4ce0c393d807e2c7472416c19b23a5
Matheus Rocha <matheusrochacamarg@gmail.com>
2020-05-04 18:50:26 -0300

    Remove da pasta aulas

--- JAVASCRIPT/aulas/aula12/ex011.js
+++
@@ -1,9 +0,0 @@
-var idade = 69
-
-if(idade < 16){
-    console.log('Não vota')
-}else if(idade < 18 || idade > 65){
-    console.log('Voto opcional')
-}else{
-    console.log('Voto obrigatorio');
-}
